4d4560d3023578010b87b7cc0803a100faa519b0
RasulAkhundov <resul.axundov2002@mail.ru>
2025-04-07 15:03:03 +0400

    new change for vercel v2

--- next.config.mjs
+++ next.config.mjs
@@ -4,18 +4,7 @@ const nextConfig = {
    images: {
       domains: ['raw.githubusercontent.com'],
    },
-   // Enable static page generation
-   output: 'standalone',
-   // Optimize page loading
-   swcMinify: true,
-   // Enable page compression
-   compress: true,
-   // Configure trailing slashes
-   trailingSlash: false,
-   // Enable automatic static optimization
-   experimental: {
-      optimizeCss: true
-   }
+   swcMinify: true
 };
 
 export default nextConfig;
