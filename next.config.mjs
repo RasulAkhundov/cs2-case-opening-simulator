cb7bc826d97584bf49cb85c754e20e56c1eaf913
RasulAkhundov <resul.axundov2002@mail.ru>
2025-04-07 15:06:04 +0400

    new change for vercel v3

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,14 @@ const nextConfig = {
    images: {
       domains: ['raw.githubusercontent.com'],
    },
-   swcMinify: true
+   swcMinify: true,
+   i18n: {
+      locales: ['en', 'tr', 'de', 'fr', 'es-Es', 'es-MX', 'pt-BR', 'pt-PT', 'zh-CN', 'zh-TW',
+      'ja', 'ko', 'ru', 'uk', 'vi', 'bg', 'cs', 'da', 'el', 'fi', 'hu', 'it', 'nl',
+      'no', 'pl', 'ro', 'sk', 'sv', 'th'],
+      defaultLocale: 'en',
+      localeDetection: true
+   }
 };
 
 export default nextConfig;
